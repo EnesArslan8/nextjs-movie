5ec26b3807c5d522ee0c84a6db1231f5d536cf55
EnesArslan8 <enes8arslan@gmail.com>
2023-08-17 17:26:37 +0300

    clicked movie compenent added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,23 @@
+import Movies from "@/components/Movies";
+import React from "react";
+export default async function Home({ searchParams }) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      searchParams.genre ? "movie/" + searchParams.genre : "trending/all/day"
+    }?api_key=c822fda16959348dcc164adaed2d503b&language=en-US&page=1`,
+    { next: { revalidate: 1000 } }
+  );
 
-export default function Home() {
+  const data = await res.json();
+
+  console.log(searchParams.genre);
   return (
-    <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
-  )
+    <div className="flex justify-center items-center flex-wrap gap-y-20 gap-x-5 ">
+      
+        {data?.results?.map((item, id) => (
+          <Movies key={id} item={item} />
+        ))}
+      </div>
+    
+  );
 }
